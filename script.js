b4a30151a4ea1d8e1afd05a867d3f29938fe29dc
ebarriosc1 <41806406+ebarriosc1@users.noreply.github.com>
2026-01-22 13:21:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const apiKey = 'AIzaSyCny_ZvhQG8HXa61-SDZRoVmxsFp2b3d80';
+const apiKey = 'AIzaSyCkpcXFQcnYtCinfQOqX7QGKg1IBDklCVw';
+
 const GEMINI_GENERATE_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
 // const DEFAULT_MODEL = 'deepseek-r1:1.5b'; 
