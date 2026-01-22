6da1f06476ae29b6eb9cedd4598be92b6e6bd27a
ebarriosc1 <41806406+ebarriosc1@users.noreply.github.com>
2026-01-22 13:24:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyCkpcXFQcnYtCinfQOqX7QGKg1IBDklCVw';
+const apiKey = 'AIzaSyClbbIfFXMq_2oCjNXBDpk6NarNbhXqdY0';
 
 const GEMINI_GENERATE_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
